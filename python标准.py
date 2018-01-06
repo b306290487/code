c4b8f91b1ee77bab8c9bbe3ad129902dcc1022a0
b306290487 <35050382+b306290487@users.noreply.github.com>
2018-01-07 00:07:56 +0800

    Add files via upload

---
+++ python标准.py
@@ -0,0 +1,32 @@
+while int print input 都是BIF（内置函数）
+dir(__builtins__) #查看内置函数
+help(input) #内置函数 说明
+print(r"c:\new\int\open") #原始字符串
+str = """  """  #打印段落
+
+import random   random.randint(1,10)
+
+small = x if x < y else y #三元操作
+
+assert 3 > 4 #断言 抛出异常
+
+for i in "word":
+	  print(i,end='')
+	 
+list(range(5))
+
+for i in range(1,10,2):
+
+print("攻击力：%d, 防御力：%d," %(200,180)) #字符串格式化
+
+string.strip(str)#默认去空格
+dir(object)#查看对象能操作的所有方法
+for i in range(list.count("返回表中该值的数量")):
+    list.remove("该值")
+import sys
+sys.argv #执行脚本传入参数
+“a”in [a,b,c] #true 包含 not in
+  
+
+
+
