58089af7c1fe018ddaab74aa94fc615af0c4b116
b306290487 <35050382+b306290487@users.noreply.github.com>
2018-01-09 21:18:53 +0800

    Update python爬虫环境搭建.py

--- python爬虫环境搭建.py
+++ python爬虫环境搭建.py
@@ -4,4 +4,5 @@ pip install BeautifulSoup4  #网页属性控制
 pip install jupyter         #编辑器
 pip install pandas   	    #table以表格形式浏览
 pip install openpyxl        #保存为excel文件 后缀xlsx
+pip install lxml   # lxml 网页内容解析 比python 自带的html.parser好且快
 jupyter notebook
