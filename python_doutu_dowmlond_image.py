1910309edda862296152919e9573439f4383502e
b306290487 <35050382+b306290487@users.noreply.github.com>
2018-01-10 22:10:42 +0800

    Create python_doutu_dowmlond_image.py

---
+++ python_doutu_dowmlond_image.py
@@ -0,0 +1,81 @@
+import requests
+import lxml
+import urllib
+import threading
+from bs4 import BeautifulSoup
+from urllib import request
+import os
+
+PAGE_URL = 'http://www.doutula.com/article/list/?page='
+#页面的url列表
+PAGE_URL_LIST = []
+#所有表情的url列表
+FACE_URL_LIST = []
+#全局锁
+
+page = 2
+pagenum = 40
+gLock = threading.Lock()
+aLock = threading.Lock()
+count = 0
+znum = (page-1)*pagenum
+
+def procuder():
+    while True:
+        gLock.acquire()
+        listlen = len(PAGE_URL_LIST)
+        gLock.release()
+        if listlen == 0:
+            break
+        else:
+            gLock.acquire()
+            page_url = PAGE_URL_LIST.pop()
+            gLock.release()
+            res = requests.get(page_url)
+            content = res.content
+            soup = BeautifulSoup(content,'html.parser')
+            img_list = soup.find_all('img',attrs={'class':'lazy image_dtb img-responsive'})
+            gLock.acquire()
+            for img in img_list:
+                url = img['data-original']
+                FACE_URL_LIST.append(url)
+            gLock.release()
+
+def customer():
+    global count
+    while True:
+        aLock.acquire()
+        listlen = len(FACE_URL_LIST)
+        if znum <= count:
+            aLock.release()
+            break
+        elif listlen == 0:
+            aLock.release()
+            continue
+        else:
+            count = count +1
+#             print(count)
+            face_url = FACE_URL_LIST.pop()
+            aLock.release()
+            filename = face_url.split('/').pop()
+            path = os.path.join('D:\images',filename)
+            request.urlretrieve(face_url,filename= path)
+
+def startThread():
+    #创建两个多线程来作为生产者，去爬取表情的url
+    for x in range(3):
+        th = threading.Thread(target = procuder)
+        th.start()
+
+
+#     创建4个线程作为消费者，去吧表情图片url下载下来
+    for x in range(1):
+        th = threading.Thread(target = customer)
+        th.start()
+        
+    
+for x in range(1,page):
+    newurl = PAGE_URL + str(x)
+    PAGE_URL_LIST.append(newurl)
+
+startThread()
